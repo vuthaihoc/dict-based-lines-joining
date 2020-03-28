9781ec4b826ac02a67c31830f1e7eaf211c5330f
Vu Thai Hoc <vuthaihoc.ttud@gmail.com>
2020-03-28 15:45:30 +0700

    simple solution

--- src/Joiner.php
+++ src/Joiner.php
@@ -16,17 +16,36 @@ class Joiner {
     
     protected $wiki_api = "https://vi.wikipedia.org/w/index.php?sort=relevance&search=%22__query__%22&ns0=1";
     
+    /**
+     * Ý tưởng: Sử dụng wikipedia search chính xác cụm từ nói string1 và string2,
+     * nếu xuất hiện chuỗi chính xác trong kết quả tìm kiếm thì trả về true
+     */
     public function shouldJoin($string1, $string2) : bool {
-        /**
-         * Ý tưởng: Sử dụng wikipedia search chính xác cụm từ nói string1 và string2,
-         * nếu xuất hiện chuỗi chính xác trong kết quả tìm kiếm thì trả về true
-         */
-        return true;
+        try{
+            $query = $string1 . " " . $string2;
+            $query = trim( $query, ".-:;,");
+            $crawler = $this->getHtml( $string1 . " " . $string2 );
+            $results = $crawler->filter( "ul.mw-search-results li div.searchresult");
+            /** @var \DOMElement $result */
+            foreach ($results as $result){
+                if(strpos( $result->textContent, $query ) !== false){
+                    return true;
+                }
+            }
+        }catch (\Exception $ex){
+            
+        }
+        
+        return false;
     }
     
     protected function getHtml($query){
         $url = str_replace( "__query__", urlencode( $query ), $this->wiki_api );
         $response = (new Client())->get($url);
+        $html = $response->getBody()->getContents();
+        $crawler = new Crawler();
+        $crawler->addHtmlContent( $html );
+        return $crawler;
     }
     
 }
--- test.php
+++ test.php
@@ -6,17 +6,20 @@
  * Time: 15:37
  */
 
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+
 require __DIR__ . "/vendor/autoload.php";
 
 $file = __DIR__ . "/vn.001.pdf";
 
 $converter = new HocVT\PdfToText\Converter();
 
-$page = 3;
+$page = 5;
 
 $text = $converter->run( $file, $page );
 
-$lines = explode( "\n", $text);
+$lines = explode( "\n", $text );
 
 $last_line = '';
 
@@ -24,28 +27,26 @@ $result = "";
 
 $joiner = new \HocVT\PdfToText\Joiner();
 
-foreach ($lines as $line){
-    if($last_line && $line){
-        $words1 = explode( " ", $last_line);
-        $words2 = explode( " ", $line);
+foreach ( $lines as $line ) {
+    if ( $last_line && $line ) {
+        $words1 = explode( " ", $last_line );
+        $words2 = explode( " ", $line );
         
-        if(count( $words1 ) < 3){
+        if ( count( $words1 ) < 3 ) {
             $string1 = $last_line;
-        }else{
-            $string1 = implode( " ", array_slice( $words1, -2));
+        } else {
+            $string1 = implode( " ", array_slice( $words1, - 2 ) );
         }
-        if(count( $words2 ) < 3){
+        if ( count( $words2 ) < 3 ) {
             $string2 = $line;
-        }else{
-            $string2 = implode( " ", array_slice( $words2, 0, 2));
+        } else {
+            $string2 = implode( " ", array_slice( $words2, 0, 2 ) );
         }
-        
-        dump([$string1, $string2], $joiner->shouldJoin( $string1, $string2));
-        $result .= ($joiner->shouldJoin( $string1, $string2) ? " " : "\n") . $line;
-    }else{
-        $result .= $line;
+        $result .= ( $joiner->shouldJoin( $string1, $string2 ) ? " " : "\n" ) . $line;
+    } else {
+        $result .= " \n" . $line;
     }
     $last_line = $line;
 }
 
-dump($result);
+dump( $text, "===============", $result );
